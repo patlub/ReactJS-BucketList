3580cc39435bd857ef5ae18610b3da1dd3f5df5e
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-13 01:46:49 +0300

    Add register Ui

--- src/components/register.js
+++ src/components/register.js
@@ -1,23 +1,35 @@
 import React, {Component} from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Link
-} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import '../App.css';
 
 class Register extends Component {
     render() {
         return (
-            <div className="App">
-                <p className="App-intro">
-                    <form onSubmit={this.onSubmitClick.bind(this)}>
-                        <input type="text" placeholder="Name" ref="name"/>
-                        <input type="text" placeholder="Email" ref="email"/>
-                        <input type="password" placeholder="Password" ref="password"/>
-                        <button>Submit</button>
-                    </form>
-                </p>
+            <div className="col-md-4">
+                <form>
+                    <div className="modal-body">
+                        <div className="form-group">
+                            <input className="form-control" type="text"
+                                   placeholder="Name" required/>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type="email" placeholder="Email" required/>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type="password" placeholder="Password"
+                                   required/>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div>
+                            <button type="submit" className="btn btn-primary btn-lg btn-block">Register</button>
+                        </div>
+                        <div>
+                            <Link to="login" type="button" className="btn btn-link">Login</Link>
+                            <Link to="forgot-password" type="button" className="btn btn-link">Forgot password</Link>
+                        </div>
+                    </div>
+                </form>
             </div>
         );
     }
