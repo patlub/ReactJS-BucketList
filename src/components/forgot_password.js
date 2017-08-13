04b039fa3afa2a5c9e71570378248a9b62b942a2
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-13 03:39:16 +0300

    Add bootstrap panel to forgot password form

--- src/components/forgot_password.js
+++ src/components/forgot_password.js
@@ -6,28 +6,36 @@ class ForgotPassword extends Component {
 
     render() {
         return (
-            <div className="col-md-4">
-                <form>
-                    <div className="modal-body">
-                        <div className="form-group">
+            <div className="container-fluid text-center ">
+                <div className="col-md-4 col-md-offset-4 col-sm-9 auth-box" align="center">
+                    <div className="panel panel-primary">
+                        <div className="panel-heading">
+                            <h4 class="panel-title">Login or Sign up</h4>
+                        </div>
+                        <div className="panel-body">
+                            <form>
+                                <div className="modal-body">
+                                    <div className="form-group">
 
-                            <input className="form-control" type="text"
-                                   placeholder="Email" required/>
-                        </div>
+                                        <input className="form-control" type="text"
+                                               placeholder="Email" required/>
+                                    </div>
 
-                    </div>
-                    <div class="modal-footer">
-                        <div>
-                            <button type="submit" className="btn btn-primary btn-lg btn-block">Send</button>
+                                </div>
+                                <div class="modal-footer">
+                                    <div>
+                                        <button type="submit" className="btn btn-primary btn-lg btn-block">Send</button>
+                                    </div>
+                                    <div>
+                                        <Link to="login" type="button" className="btn btn-link">Login</Link>
+                                        <Link to="register" type="button" className="btn btn-link">Register</Link>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        <div>
-                            <Link to="login" type="button" className="btn btn-link">Login</Link>
-                            <Link to="register" type="button" className="btn btn-link">Register</Link>
-                        </div>
                     </div>
-                </form>
+                </div>
             </div>
-
         );
     }
 }
