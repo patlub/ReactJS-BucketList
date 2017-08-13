70ff1acfb7a839278e6b3b7649b915c9fff8e7f7
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-13 03:36:58 +0300

    Add panel to form

--- src/components/login.js
+++ src/components/login.js
@@ -6,32 +6,42 @@ import $ from 'jquery'
 class Login extends Component {
     render() {
         return (
-            <div className="col-md-4">
-                <form onSubmit={this.onLoginHandler.bind(this)}>
-                    <div className="modal-body">
-                        <div className="form-group">
-                            <input className="form-control" type="email"
-                                   placeholder="email" ref="login_email" required/>
+            <div className="container-fluid text-center ">
+                <div className="col-md-4 col-md-offset-4 col-sm-9 auth-box" align="center">
+                    <div className="panel panel-primary">
+                        <div className="panel-heading">
+                            <h4 class="panel-title">Login or Sign up</h4>
                         </div>
-                        <div className="form-group">
-                            <input className="form-control" type="password" placeholder="Password"
-                                   ref="login_password" required/>
-                        </div>
+                        <div className="panel-body">
+                            <form onSubmit={this.onLoginHandler.bind(this)}>
+                                <div className="modal-body">
+                                    <div className="form-group">
+                                        <input className="form-control" type="email"
+                                               placeholder="Email" ref="login_email" required/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input className="form-control" type="password" placeholder="Password"
+                                               ref="login_password" required/>
+                                    </div>
 
-                    </div>
-                    <div class="modal-footer">
-                        <div>
-                            <button type="submit" className="btn btn-primary btn-lg btn-block">Login</button>
-                        </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <div>
+                                        <button type="submit" className="btn btn-primary btn-lg btn-block">Login
+                                        </button>
+                                    </div>
 
-                        <div>
-                            <Link to="register" type="button" className="btn btn-link">Register</Link>
-                            <Link to="forgot-password" type="button" className="btn btn-link">Forgot password</Link>
+                                    <div>
+                                        <Link to="register" type="button" className="btn btn-link">Register</Link>
+                                        <Link to="forgot-password" type="button" className="btn btn-link">Forgot
+                                            password</Link>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
-
         );
     }
 
