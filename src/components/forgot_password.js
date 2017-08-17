fdf01b7a838b2f57771b74c39b96b641ec8f4986
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-17 14:31:35 +0300

    Add links to forgot password

--- src/components/forgot_password.js
+++ src/components/forgot_password.js
@@ -10,7 +10,7 @@ class ForgotPassword extends Component {
                 <div className="col-md-4 col-md-offset-4 col-sm-9 auth-box" align="center">
                     <div className="panel panel-primary">
                         <div className="panel-heading">
-                            <h4 class="panel-title">Login or Sign up</h4>
+                            <h4 className="panel-title">Forgot Password</h4>
                         </div>
                         <div className="panel-body">
                             <form>
@@ -22,13 +22,13 @@ class ForgotPassword extends Component {
                                     </div>
 
                                 </div>
-                                <div class="modal-footer">
+                                <div className="modal-footer">
                                     <div>
                                         <button type="submit" className="btn btn-primary btn-lg btn-block">Send</button>
                                     </div>
                                     <div>
-                                        <Link to="login" type="button" className="btn btn-link">Login</Link>
-                                        <Link to="register" type="button" className="btn btn-link">Register</Link>
+                                        <Link to="login" className="btn btn-link">Login</Link>
+                                        <Link to="register" className="btn btn-link">Register</Link>
                                     </div>
                                 </div>
                             </form>
