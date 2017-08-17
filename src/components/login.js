d4b128c8419ff3769eababcb60f8d85304e4a3c1
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-17 14:28:56 +0300

    Set states and redirect

--- src/components/login.js
+++ src/components/login.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
 import '../App.css';
-import {Link} from 'react-router-dom'
+import {Link, Redirect} from 'react-router-dom'
 import $ from 'jquery'
+import Bucket from './buckets'
+
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false
+        };
+    }
+
     render() {
+        if (this.state.loggedIn) {
+            return <Redirect to="/buckets"/>
+        }
+
         return (
             <div className="container-fluid text-center ">
-                <div className="col-md-4 col-md-offset-4 col-sm-9 auth-box" align="center">
+                <div className="col-md-4 col-md-offset-4 col-sm-9 auth-box">
                     <div className="panel panel-primary">
                         <div className="panel-heading">
-                            <h4 class="panel-title">Login or Sign up</h4>
+                            <h4 className="panel-title">Login</h4>
                         </div>
                         <div className="panel-body">
                             <form onSubmit={this.onLoginHandler.bind(this)}>
@@ -25,19 +38,18 @@ class Login extends Component {
                                     </div>
 
                                 </div>
-                                <div class="modal-footer">
+                                <div className="modal-footer">
                                     <div>
                                         <button type="submit" className="btn btn-primary btn-lg btn-block">Login
                                         </button>
                                     </div>
-
                                     <div>
-                                        <Link to="register" type="button" className="btn btn-link">Register</Link>
-                                        <Link to="forgot-password" type="button" className="btn btn-link">Forgot
-                                            password</Link>
+                                        <Link to="register" className="btn btn-link">Register</Link>
+                                        <Link to="forgot_password" className="btn btn-link">Forgot Password</Link>
                                     </div>
                                 </div>
                             </form>
+
                         </div>
                     </div>
                 </div>
@@ -49,20 +61,20 @@ class Login extends Component {
         event.preventDefault();
         const email = this.refs.login_email.value;
         const password = this.refs.login_password.value;
+        this.setState({loggedIn: true});
 
-        $.ajax({
-            type: "POST",
-            url: 'https://patrickluboobi-bucket-list-api.herokuapp.com/auth/login',
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
-            async: false,
-            data: JSON.stringify({email: email, password: password}),
-            success: function (response) {
-                console.log(response['id']);
-                window.location = '/buckets';
-            }
-        })
-
+        // $.ajax({
+        //     type: "POST",
+        //     url: 'https://patrickluboobi-bucket-list-api.herokuapp.com/auth/login',
+        //     dataType: 'json',
+        //     contentType: 'application/json; charset=utf-8',
+        //     async: false,
+        //     data: JSON.stringify({email: email, password: password}),
+        //     success: function (response) {
+        //         this.setState({loggedIn: true});
+        //         // window.location = '/buckets';
+        //     }.bind(this)
+        // })
     }
 
 }
