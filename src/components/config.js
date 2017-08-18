c567067145c5bf9e5e6c05784abcdc2820a9b0ee
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-18 23:24:06 +0300

    Add Authorization header to axios

--- src/components/config.js
+++ src/components/config.js
@@ -2,8 +2,10 @@ import axios from 'axios'
 
 
 const axiosInstance = axios.create({
-    timeout: 10000,
+    // timeout: 10000,
     baseURL: 'https://patrickluboobi-bucket-list-api.herokuapp.com',
+    headers: {'Authorization': localStorage.getItem('token')}
+
 });
 
 export default axiosInstance
