7ddef57da1297c1cf69d33bdbbddb3a8f4051cdb
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-18 23:32:41 +0300

    Apply eslint styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
-
-    render() {
-        return (
-            <div className="App">
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App" />
+    );
+  }
 }
 
 
