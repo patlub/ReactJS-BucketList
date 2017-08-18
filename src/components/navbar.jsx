223b8ad75ec5ffe4ebde5d275615378f156c7dfd
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-18 23:29:02 +0300

    Make navbar responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import '../App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+
 
 class NavBar extends Component {
     render() {
         return (
             <div className="row app-nav">
-                <div className="col-md-9 pull-left">
-                    <h2 id="app-title">BucketList</h2>
+                <div className="col-md-8 col-sm-8 pull-left">
+                    <h3 id="app-title">BucketList</h3>
                 </div>
+                <div className="col-md-2 col-sm-2 nav-tab text-center">
+                    <a href="#" data-toggle="modal" data-target="#addBucketModal">
+                        Add Bucket
+                    </a>
 
+                </div>
+                <div className="col-md-2 col-sm-2 nav-tab text-center">
+                    <Link to="logout">Logout</Link>
+                </div>
             </div>
         );
     }
