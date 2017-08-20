ad648db7a9c4a05b23ad9f1cb5ac4601d9609693
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-20 20:56:48 +0300

    test with localhost

--- src/components/config.js
+++ src/components/config.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
     // timeout: 10000,
-    baseURL: 'https://patrickluboobi-bucket-list-api.herokuapp.com',
+    // baseURL: 'https://patrickluboobi-bucket-list-api.herokuapp.com',
+    baseURL: 'http://127.0.0.1:5000',
     headers: {'Authorization': localStorage.getItem('token')}
 
 });
