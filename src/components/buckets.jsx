5ee86d14f3ea481dde130a3ca1b037643b7075f2
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-20 20:49:10 +0300

    Fetch buckets into array and setState

--- src/components/buckets.jsx
+++ src/components/buckets.jsx
@@ -4,24 +4,30 @@ import AddBucket from './add_bucket'
 import '../App.css';
 import NavBar from './navbar'
 import axiosInstance from './config';
+import _ from 'lodash'
 
+const bucketLists = [];
 
 class Buckets extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            token: localStorage.getItem('token')
+            token: localStorage.getItem('token'),
+            buckets: false
         };
     }
 
     render() {
-        console.log(localStorage.getItem('token'));
+        if (!this.state.buckets) {
+            this.getBuckets();
+        }
         if (this.state.token) {
             return (
                 <div>
                     <NavBar/>
-                    <AddBucket/>
+                    <AddBucket buckets={this.state.buckets} addBucket={this.addBucket.bind(this)}/>
                 </div>
+
             );
         } else {
             return <Redirect to="/login"/>
@@ -31,11 +37,63 @@ class Buckets extends Component {
     getBuckets() {
         axiosInstance.get('/buckets')
             .then(function (response) {
-                console.log(response)
-            })
+                _.forEach(response.data, function (value) {
+                    bucketLists.push(value);
+                });
+                console.log(bucketLists);
+                this.setState({buckets: bucketLists});
+            }.bind(this))
             .catch(function (error) {
                 console.log(error);
             });
+    }
+
+    addBucket(bucket) {
+        console.log(this.state.buckets);
+        bucketLists.push(bucket);
+        this.setState({bucket: bucketLists})
+    }
+
+    render_buckets(buckets) {
+        return (
+            <div className="panel panel-primary">
+                <div className="panel-heading">
+                    <h4 className="panel-title">Login</h4>
+                </div>
+                <div className="panel-body">
+                    <form onSubmit={this.onLoginHandler.bind(this)}>
+                        <div className="modal-body">
+                            <div className="form-group">
+                                <input
+                                    className="form-control"
+                                    type="email"
+                                    placeholder="Email"
+                                    ref="login_email"
+                                    required
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    className="form-control"
+                                    type="password"
+                                    placeholder="Password"
+                                    ref="login_password"
+                                    required
+                                />
+                            </div>
+
+                        </div>
+                        <div className="modal-footer">
+                            <div>
+                                <button type="submit" className="btn btn-primary btn-lg btn-block">Login
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        );
     }
 }
 
