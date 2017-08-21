cf03e95a4f925274e5a5fdac177cfa43d7981ae3
Patrick Luboobi <patricklu2010@gmail.com>
2017-08-21 16:04:39 +0300

    Fix add bucket method

--- src/components/add_bucket.jsx
+++ src/components/add_bucket.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import '../App.css';
-import {Link, Redirect} from 'react-router-dom';
 import axiosInstance from './config';
 
 class AddBucket extends Component {
