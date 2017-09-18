9acb7e727d524e290112d005049d801acf7fdcd0
patlub <patricklu2010@gmail.com>
2017-09-18 23:10:23 +0300

    Refactor code

--- src/components/BucketList.jsx
+++ src/components/BucketList.jsx
@@ -7,95 +7,26 @@ class BucketList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isEditing: false
+            isEditing: false,
+            bucket: this.props.name,
+            desc: this.props.desc,
         };
     }
 
-    renderActions() {
-        if (this.state.isEditing) {
-            return (
-                <td>
-                    <button onClick={this.onSaveClick.bind(this)} className="btn btn-sm btn-success action-btn">Save
-                    </button>
-                    <button onClick={this.onCancelClick.bind(this)} className="btn btn-sm btn-default">Cancel</button>
-                </td>
-            );
-        }
-        return (
-            <td>
-                <button onClick={this.onEditClick.bind(this)} className="btn btn-sm btn-primary action-btn">Edit
-                </button>
-                <button onClick={this.onDeleteClick.bind(this)} className="btn btn-sm btn-danger">Delete</button>
-            </td>
-        );
-    }
+    /*
+    * Fired when input changes
+    * @param (event) event when input changes
+    * */
+    onInputChanged = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    };
 
-    renderBuckets() {
-        if (this.state.isEditing) {
-            return (
-                <tr>
-                    <td><input type="text" defaultValue={this.props.name} ref="editBucketName"/></td>
-                    <td><input type="text" defaultValue={this.props.desc} ref="editDesc"/></td>
-                    <td>{this.props.date_added}</td>
-                    {this.renderActions()}
-                </tr>
-            );
-        }
-
-        return (
-            <tr>
-                <td onClick={this.onViewItemClick.bind(this)}>{this.props.name}</td>
-                <td>{this.props.desc}</td>
-                <td>{this.props.date_added}</td>
-                {this.renderActions()}
-            </tr>
-        );
-    }
-
-    render() {
-        return this.renderBuckets();
-    }
-
-    onEditClick() {
-        this.setState({isEditing: true})
-    }
-
-    onCancelClick() {
-        this.setState({isEditing: false})
-    }
-
-    onSaveClick(event) {
-        event.preventDefault();
-        const bucket = this.refs.editBucketName.value;
-        const desc = this.refs.editDesc.value;
-
-        axiosInstance.put(`/buckets/${this.props.id}`,
-            {
-                bucket,
-                desc
-            })
-            .then((response) => {
-                this.setState({isEditing: false});
-                this.props.updateBuckets(this.props.id, bucket, desc);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-
-    onDeleteClick(event) {
-        event.preventDefault();
-        axiosInstance.delete(`/buckets/${this.props.id}`)
-            .then((response) => {
-                this.setState({isEditing: false});
-                this.props.deleteBucket(this.props.id);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-
-    onViewItemClick(event) {
+    /**
+     * This method handles viewing a bucket.
+     */
+    onViewBucketClick = () => {
         const bucketListItems = [];
         axiosInstance.get(`/items/${this.props.id}`)
             .then(function (response) {
@@ -109,9 +40,109 @@ class BucketList extends Component {
             .catch(function (error) {
                 console.log(error);
             });
+    };
 
+    /**
+     * This method handles saving an updated a bucket.
+     * @param {event} event when triggering an onSaveClick action.
+     */
+    onSaveClick = (event) => {
+        event.preventDefault();
+        axiosInstance.put(`/buckets/${this.props.id}`,
+            {
+                bucket: this.state.bucket,
+                desc: this.state.desc,
+            })
+            .then((response) => {
+                this.setState({isEditing: false});
+                this.props.updateBuckets(this.props.id, this.state.bucket, this.state.desc);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    /**
+     * This method handles deleting a bucket.
+     */
+    onDeleteClick = (event) => {
+        event.preventDefault();
+        axiosInstance.delete(`/buckets/${this.props.id}`)
+            .then((response) => {
+                this.setState({isEditing: false});
+                this.props.deleteBucket(this.props.id);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    onEditClick = () => {
+        this.setState({isEditing: true})
+    };
+
+    onCancelClick = () => {
+        this.setState({isEditing: false})
+    };
+
+    renderActions = () => {
+        if (this.state.isEditing) {
+            return (
+                <td>
+                    <button onClick={this.onSaveClick} className="btn btn-sm btn-success action-btn">Save
+                    </button>
+                    <button onClick={this.onCancelClick} className="btn btn-sm btn-default">Cancel</button>
+                </td>
+            );
+        }
+        return (
+            <td>
+                <button onClick={this.onEditClick} className="btn btn-sm btn-primary action-btn">Edit
+                </button>
+                <button onClick={this.onDeleteClick} className="btn btn-sm btn-danger">Delete</button>
+            </td>
+        );
+    };
+
+    renderBuckets = () => {
+        if (this.state.isEditing) {
+            return (
+                <tr>
+                    <td>
+                        <input
+                            type="text"
+                            name="bucket"
+                            defaultValue={this.state.bucket}
+                            value={this.state.bucket}
+                            onChange={this.onInputChanged}/>
+                    </td>
+                    <td>
+                        <input
+                            type="text"
+                            name="desc"
+                            defaultValue={this.state.bucket}
+                            value={this.state.desc}
+                            onChange={this.onInputChanged}/>
+                    </td>
+                    <td>{this.props.date_added}</td>
+                    {this.renderActions()}
+                </tr>
+            );
+        }
+
+        return (
+            <tr>
+                <td onClick={this.onViewBucketClick}>{this.props.name}</td>
+                <td>{this.props.desc}</td>
+                <td>{this.props.date_added}</td>
+                {this.renderActions()}
+            </tr>
+        );
+    };
+
+    render() {
+        return this.renderBuckets();
     }
-
 }
 
 export default BucketList;
