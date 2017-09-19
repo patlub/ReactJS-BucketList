8cf8cd4f0be51d4c4f32912715759633966cf0fb
patlub <patricklu2010@gmail.com>
2017-09-19 15:12:46 +0300

    Change class component to function component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,24 +3,20 @@ import {Link} from 'react-router-dom';
 import '../App.css';
 
 
-class NavBar extends Component {
-    render() {
-        return (
-            <div className="row app-nav">
-                <div className="col-md-8 col-sm-8 pull-left">
-                    <h3 id="app-title">BucketList</h3>
-                </div>
-                <div className="col-md-2 col-sm-2 nav-tab text-center">
-                    <a href="#" data-toggle="modal" data-target="#addBucketModal">
-                        Add Bucket
-                    </a>
+const NavBar = () => (
+    <div className="row app-nav">
+        <div className="col-md-8 col-sm-8 pull-left">
+            <h3 id="app-title">BucketList</h3>
+        </div>
+        <div className="col-md-2 col-sm-2 nav-tab text-center">
+            <a href="#" data-toggle="modal" data-target="#addBucketModal">
+                Add Bucket
+            </a>
 
-                </div>
-                <div className="col-md-2 col-sm-2 nav-tab text-center">
-                    <Link to="logout">Logout</Link>
-                </div>
-            </div>
-        );
-    }
-}
+        </div>
+        <div className="col-md-2 col-sm-2 nav-tab text-center">
+            <Link to="logout">Logout</Link>
+        </div>
+    </div>
+);
 export default NavBar;
