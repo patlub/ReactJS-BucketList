44913958fa0eca65525606db5fe856f1b82f21e4
patlub <patricklu2010@gmail.com>
2017-09-19 15:12:37 +0300

    Change class component to function component

--- src/components/ItemTableHeader.jsx
+++ src/components/ItemTableHeader.jsx
@@ -1,19 +1,14 @@
 import React, {Component} from 'react';
 import '../App.css';
 
-class ItemTableHeader extends Component {
-    render() {
-        return (
-            <thead>
-            <tr>
-                <th>Item</th>
-                <th>Status</th>
-                <th>Date Added</th>
-                <th>Action</th>
-            </tr>
-            </thead>
-        );
-    }
-
-}
+const ItemTableHeader = () => (
+    <thead>
+    <tr>
+        <th>Item</th>
+        <th>Status</th>
+        <th>Date Added</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+);
 export default ItemTableHeader;
