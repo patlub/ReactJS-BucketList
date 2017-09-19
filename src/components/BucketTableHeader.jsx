bdcb7a8faf9a57e5e5cc7669ce94872906966594
patlub <patricklu2010@gmail.com>
2017-09-19 15:12:13 +0300

    Change class component to function component

--- src/components/BucketTableHeader.jsx
+++ src/components/BucketTableHeader.jsx
@@ -1,19 +1,16 @@
 import React, {Component} from 'react';
 import '../App.css';
 
-class BucketTableHeader extends Component {
-    render() {
-        return (
-            <thead>
-            <tr>
-                <th>Bucket name</th>
-                <th>Description</th>
-                <th>Date Added</th>
-                <th>Action</th>
-            </tr>
-            </thead>
-        );
-    }
+const BucketTableHeader = () => (
+    <thead>
+    <tr>
+        <th>Bucket name</th>
+        <th>Description</th>
+        <th>Date Added</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+);
 
-}
+
 export default BucketTableHeader;
