4f1a71f05ac3db5e10fdb3e8144548f21c0d175d
patlub <patricklu2010@gmail.com>
2017-09-20 21:23:34 +0300

    Remove axiosInstance

--- src/components/config.js
+++ src/components/config.js
@@ -1,15 +1,5 @@
-import axios from 'axios'
 
-
-const axiosInstance = axios.create({
-    // timeout: 10000,
-    // baseURL: 'https://patrickluboobi-bucket-list-api.herokuapp.com',
-    baseURL: 'http://127.0.0.1:5000',
-    headers: {'Authorization': localStorage.getItem('token')}
-
-});
-
-export default axiosInstance
 /**
  * Created by PATRICK on 8/17/2017.
  */
+export const baseURL =  'http://127.0.0.1:5000';
