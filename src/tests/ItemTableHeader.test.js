ed452777932fecb7464863c9ed62647a562c1cb5
patlub <patricklu2010@gmail.com>
2017-09-22 07:26:10 +0300

    add tests for stateless components

---
+++ src/tests/ItemTableHeader.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer'
+import ItemTableHeader from '../components/ItemTableHeader';
+
+
+it('Displays item table header', () => {
+    const rendered = renderer.create(
+        <ItemTableHeader/>,
+    );
+    expect(rendered.toJSON()).toMatchSnapshot();
+});
