7432d91497379dcc082f67cf63e888a36c7b5c9a
patlub <patricklu2010@gmail.com>
2017-09-26 15:42:58 +0300

    Add axios mocks to test authentication

--- src/tests/authentication/Login.test.jsx
+++ src/tests/authentication/Login.test.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import ReactTestUtils from 'react-dom/test-utils'; // ES6
 import { StaticRouter } from 'react-router';
 import Login from '../../authentication/Login';
 import renderer from 'react-test-renderer';
+import { baseURL } from '../../configs/config';
 
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+
+// This sets the mock adapter on the default instance
+const mock = new MockAdapter(axios);
+// Mock Login POST request to /login
+mock.onPost(`${baseURL}/auth/login`).reply(200, {
+  users: [
+    { status: 'success', token: 1 },
+  ],
+});
 
 global.localStorage = {
   getItem: () => {},
+  setItem: () => {},
 };
 
 describe('Component: Login', () => {
+  const routerComponent = mount(
+    <StaticRouter location="login" context={{}}>
+      <Login />
+    </StaticRouter>,
+  );
+  const loginComponent = routerComponent.find('Login');
+  const emailInput = loginComponent.find('[type="email"]');
+  const passwordInput = loginComponent.find('[type="password"]');
+
   it('Displays Login', () => {
     const rendered = renderer.create(
-      <StaticRouter location="login" context={''}>
+      <StaticRouter location="login" context={{}}>
         <Login />
       </StaticRouter>,
     );
@@ -21,19 +43,15 @@ describe('Component: Login', () => {
   });
 
   it('updates state on input change', () => {
-    const routerComponent = mount(
-      <StaticRouter location="login" context={''}>
-        <Login />
-      </StaticRouter>,
-    );
-    const loginComponent = routerComponent.find('Login');
-    const emailInput = loginComponent.find('[type="email"]');
-    const passwordInput = loginComponent.find('[type="password"]');
-
-    ReactTestUtils.Simulate.change(emailInput.getDOMNode(), { target: { name: 'email', value: 'patrick@gmail.com' } });
-    ReactTestUtils.Simulate.change(passwordInput.getDOMNode(), { target: { name: 'password', value: 'patrick' } });
+    emailInput.simulate('change', { target: { name: 'email', value: 'patrick@gmail.com' } });
+    passwordInput.simulate('change', { target: { name: 'password', value: 'patrick' } });
     expect(routerComponent.find('Login').nodes[0].state.email).toEqual('patrick@gmail.com');
     expect(routerComponent.find('Login').nodes[0].state.password).toEqual('patrick');
+  });
+
+  it('signs in user', () => {
+    const loginButton = loginComponent.find('[type="submit"]');
+    loginButton.simulate('submit');
   });
 });
 
--- src/tests/authentication/Register.test.jsx
+++ src/tests/authentication/Register.test.jsx
@@ -4,16 +4,40 @@ import ReactTestUtils from 'react-dom/test-utils'; // ES6
 import { StaticRouter } from 'react-router';
 import renderer from 'react-test-renderer';
 import Register from '../../authentication/Register';
+import { baseURL } from '../../configs/config';
+
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+
+// This sets the mock adapter on the default instance
+const mock = new MockAdapter(axios);
+// Mock Login POST request to /login
+mock.onPost(`${baseURL}/auth/register`).reply(201, {
+  users: [
+    { status: 'success', token: 1 },
+  ],
+});
 
 
 global.localStorage = {
   getItem: () => {},
+  setItem: () => {},
 };
 
 describe('Component: Register', () => {
+  const routerComponent = mount(
+    <StaticRouter location="register" context={{}}>
+      <Register />
+    </StaticRouter>,
+  );
+  const registerComponent = routerComponent.find('Register');
+  const nameInput = registerComponent.find('[type="text"]');
+  const emailInput = registerComponent.find('[type="email"]');
+  const passwordInput = registerComponent.find('[type="password"]');
+
   it('Displays Register', () => {
     const rendered = renderer.create(
-      <StaticRouter location="register" context={''}>
+      <StaticRouter location="register" context={{}}>
         <Register />
       </StaticRouter>,
     );
@@ -21,23 +45,18 @@ describe('Component: Register', () => {
   });
 
   it('updates state on input change', () => {
-    const routerComponent = mount(
-      <StaticRouter location="register" context={''}>
-        <Register />
-      </StaticRouter>,
-    );
-    const registerComponent = routerComponent.find('Register');
-    const nameInput = registerComponent.find('[type="text"]');
-    const emailInput = registerComponent.find('[type="email"]');
-    const passwordInput = registerComponent.find('[type="password"]');
-
-    ReactTestUtils.Simulate.change(nameInput.getDOMNode(), { target: { name: 'name', value: 'patrick' } });
-    ReactTestUtils.Simulate.change(emailInput.getDOMNode(), { target: { name: 'email', value: 'patrick@gmail.com' } });
-    ReactTestUtils.Simulate.change(passwordInput.getDOMNode(), { target: { name: 'password', value: 'secret' } });
+    nameInput.simulate('change', { target: { name: 'name', value: 'patrick' } });
+    emailInput.simulate('change', { target: { name: 'email', value: 'patrick@gmail.com' } });
+    passwordInput.simulate('change', { target: { name: 'password', value: 'secret' } });
 
     expect(routerComponent.find('Register').nodes[0].state.name).toEqual('patrick');
     expect(routerComponent.find('Register').nodes[0].state.email).toEqual('patrick@gmail.com');
     expect(routerComponent.find('Register').nodes[0].state.password).toEqual('secret');
+  });
+
+  it('signs up user', () => {
+    const registerButton = registerComponent.find('[type="submit"]');
+    registerButton.simulate('submit');
   });
 });
 
